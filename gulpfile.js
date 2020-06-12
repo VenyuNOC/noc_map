69e90c59a8b5440b2d391259c86de5e58b8eab3d
Nathan Mann <nathan.mann@eatel.com>
2020-06-12 05:58:09 -0500

    f&*k with toolchain

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,79 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserify = require('browserify');
+var buffer = require('vinyl-buffer');
 var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var tsify = require('tsify');
+const { createCaseBlock } = require('typescript');
 
-gulp.task('sass', function(cb) {
-  gulp
-    .src('scss/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('dist/'));
-  cb();
+
+gulp.task('copy-html', function (cb) {
+    gulp
+        .src('./src/html/**/*.html')
+        .pipe(gulp.dest('./dist/'));
+    cb();
 });
 
-gulp.task('browserify', function() {
-  gulp.src()
-
-  return browserify('js/network_map.js')
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('dist/'));
+gulp.task('copy-images', function (cb) {
+    gulp
+        .src('./node_modules/leaflet/dist/images/**/*.png')
+        .pipe(gulp.dest('./dist/images/'));
+    cb();
 });
 
-gulp.task('copy-html', function() {
-  gulp
-    .src('html/*.html')
-    .pipe(gulp.dest('dist/'));
+gulp.task('copy-statics', function (cb) {
+    gulp.parallel(
+        'copy-images',
+        'copy-html'
+    );
+    cb();
 });
 
-gulp.task('copy-images', function() {
-  gulp
-    .src('node_modules/leaflet/dist/images/*.png')
-    .pipe(gulp.dest('dist/'));
+gulp.task('sassify', function (cb) {
+    gulp
+        .src('./src/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./dist/css/'));
+    cb();
 });
 
-gulp.task(
-  'default',
-  gulp.parallel(
-    gulp.series('browserify', function(cb) {
-      gulp.watch('js/*.js', gulp.task('browserify'))
-      cb();
-    }),
-    gulp.series('copy-html', function(cb) {
-      gulp.watch('html/*.html', gulp.task('copy-html'))
-      cb()
-    }),
-    gulp.series('sass', function(cb) {
-      gulp.watch('scss/*.scss', gulp.task('sass'))
-      cb()
-    }),
-    gulp.series('copy-images', function(cb) {
-      gulp.watch('node_modules/leaflet/dist/images/*.png', gulp.task('copy-images'))
-      cb()
-    })
-  )
-);
+gulp.task('browserify', function () {
+    return browserify()
+        .add('./src/ts/network_map.ts')
+        .plugin(tsify)
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(gulp.dest('./dist/js/'))
+});
+
+gulp.task('default', function (cb) {
+    gulp.series(
+        gulp.task('watch-html', function(cb) {
+            gulp.watch('./src/html/**/*.html', gulp.task('copy-html'));
+            cb();
+        }),
+        gulp.task('watch-sass', function(cb) {
+            gulp.watch('./scss/**/*.scss', gulp.task('sassify'));
+            cb();
+        }),
+        gulp.task('watch-ts', function(cb) {
+            gulp.watch('./src/ts/**/*.ts', gulp.task('browserify'));
+            cb();
+        })  
+    );
+    cb();
+});
+
+gulp.task('build', function (cb) {
+    gulp.series(
+        'copy-statics',
+        'browserify',
+        'sassify'
+    );
+    cb();
+});
+
+gulp.task('clean', function () {
+    gulp.em
+})
--- src/ts/network_map.ts
+++ src/ts/network_map.ts
@@ -1,23 +1,22 @@
 import * as L from 'leaflet';
+import { IEndpoint } from "./models/endpoints/endpoints.types";
+// import { ICircuit } from "./models/circuits/circuits.types";
 
-function setupMap(noc_map: L.Map, focus: Models.Focus) {
+function setupMap(noc_map: L.Map, latLon: L.LatLng, zoom: number) {
     noc_map.setView(
-        {
-            lat: focus.lat, 
-            lng: focus.lon
-        },
-        focus.zoom
+        latLon,
+        zoom
     );
 
     L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
         maxZoom: 19,
-        attribution: '© OpenStreetMap contributors',
+        attribution: 'Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL.',
     }).addTo(noc_map);
 
     L.control.scale().addTo(noc_map);
 }
 
-function addEndpoints(noc_map: L.Map, endpoints: Models.Endpoint[]) {
+function addEndpoints(noc_map: L.Map, endpoints: IEndpoint[]) {
     endpoints.forEach((endpoint) => {
         L.marker({
             lat: endpoint.lat, 
@@ -31,18 +30,19 @@ function addEndpoints(noc_map: L.Map, endpoints: Models.Endpoint[]) {
 window.onload = () => {
     var noc_map = L.map('map');
 
-    setupMap(noc_map, {
-        lat: 32.287133,
-        lon: -90.197754,
-        zoom: 7
-    });
+    setupMap(
+        noc_map, 
+        new L.LatLng(32.287133, -90.197754), 
+        7
+    );
 
-    addEndpoints(noc_map, 
+    addEndpoints(
+        noc_map, 
         [
             {
                 lat: 30.45167,
                 lon: -91.11642,
-                "label": "BTR DCs & NOC"
+                label: "BTR DCs & NOC"
             },
             {
                 lat: 32.512915,
